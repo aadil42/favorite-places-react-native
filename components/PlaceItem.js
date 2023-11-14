7c0de9ff35df0f7875959cfcf6c38625ab263103
aadil42 <adilkelawala@gmail.com>
2023-11-14 10:48:20 +0530

    change text color to primary200

--- components/PlaceItem.js
+++ components/PlaceItem.js
@@ -1,15 +1,23 @@
-import { View, Image, Text, Pressable } from "react-native";
+import { View, Image, Text, Pressable, StyleSheet } from "react-native";
 
+// import constants
+import { Colors } from "../constants/Colors";
 
 const PlaceItem = () => {
     return (
         <Pressable>
             <View>
                 <Image source={require('../assets/tajMahal.jpeg')}/>
-                <Text>This is PlaceItem</Text>
+                <Text style={styles.text}>This is PlaceItem</Text>
             </View>
         </Pressable>
     );
 }
+
+const styles = StyleSheet.create({
+    text: {
+        color: Colors.primary200
+    }
+});
 
 export default PlaceItem;
