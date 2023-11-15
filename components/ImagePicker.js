fe75cc10f36c3892015272eeb6869fcc9acbf3ad
aadil42 <adilkelawala@gmail.com>
2023-11-15 14:50:57 +0530

    add code for getting permission in ios

--- components/ImagePicker.js
+++ components/ImagePicker.js
@@ -1,24 +1,46 @@
 import { useState } from "react";
 
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Alert } from "react-native";
 
 import OutlineBtn from "./OutlineBtn";
 
 
 // import imagetaking functions
-import { launchCameraAsync, useCameraPermissions } from "expo-image-picker";
+import { launchCameraAsync, 
+         useCameraPermissions, 
+         PermissionStatus } from "expo-image-picker";
 
 const ImagePicker = () => {
 
     const [imageUri, setImageUri] = useState(null);
+    const [camaraPermissionInformation, requstPermission] = useCameraPermissions();
+    
+    const varifyPermission = async () => {
+
+        if(camaraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const response = await requstPermission();
+            return response.granted;
+        }
+
+        if(camaraPermissionInformation.status === PermissionStatus.DENIED) {
+            Alert("Permission Denied", "You  must grant permission to your storage to use this feature");
+            return false;
+        }
+        
+    }
 
     const imagePickerHandler = async () => {
+
+        // check permission
+        const isAllowed = varifyPermission();
+        if(!isAllowed) return;
+
         const image = await launchCameraAsync({
             allowsEditing: true,
             aspect: [16, 9],
             quality: .5
         });
-        
+
         const {uri} = image.assets[0];
         setImageUri(uri);
     }
